47fcbf42acab1082ba541840b54d78ef0b7a94dc
Andrea Crotti <andrea.crotti.0@gmail.com>
2010-08-18 01:28:43 +0200

    two functions working fine

--- kata2.py
+++ kata2.py
@@ -6,21 +6,19 @@ import sys
 
 # the array is sorted already
 def chop_rec(n, array):
-    if len(array) == 1:
-        if array[0] == n:
-            return 0
-        else:
+    def chop_rec_inter(n, array, start, end):
+        if end < start:
             return -1
-            
-    middle = len(array) / 2
-    el = array[middle]
-    print middle, el
-    if n == el:
-        return middle
-    elif n > el:
-        return middle + chop_rec(n, array[:middle])
-    else:
-        return chop_rec(n, array[middle:])
+        mid = start + ((end - start) / 2)
+        val = array[mid]
+        if val > n:
+            return chop_rec_inter(n, array, start, end-1)
+        elif val < n:
+            return chop_rec_inter(n, array, start+1, end)
+        else:
+            return mid
+
+    return chop_rec_inter(n, array, 0, len(array) - 1)
 
 def chop_iterative(n, array):
     "simple version with increments"
@@ -37,30 +35,13 @@ def chop_iterative(n, array):
             end -= 1
         else:
             start += 1
-
-def chop_iterative_double(n, array):
-    "Smarter iterative version"
-    start, end = 0, (len(array) - 1)
-    while True:
-        if start > end:
-            return -1
-
-        mid = start + ((end - start) / 2)
-        el = array[mid]
-        if n == el:
-            return mid
-        # TODO: jump to the middle with the right checks 
-        elif n < el:
-            end -= 1
-        else:
-            start += 1
-    
-
 # 1 mistake:
 # - using the recursive version I'm going in max recurs depth very quickly
 # 2 mistake
 # - also using the iterative version I come into an infinite loop in the beginning
 # - getting wrong the indexing of the middle element
+# 3 mistake
+# - in the recursive version, calling chop_rec_inter in the other way around
 
 def test_chop(fun):
     assert(fun(2, [1,2,3,4]) == 1)
