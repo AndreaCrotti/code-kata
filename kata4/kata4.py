b93445fbcfd4ba0a470bfb607c76e96d81ce16e3
Andrea Crotti <andrea.crotti.0@gmail.com>
2011-10-02 19:13:25 +0100

    slight polishing of kata4

--- kata4/kata4.py
+++ kata4/kata4.py
@@ -1,13 +1,19 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 import re
 
+FOOTBAL_RE = re.compile(r'\s+\d+\. (?P<team>\w+).*?(?P<for>\d+)\s+-\s+(?P<ag>\d+).*')
+WEATHER_RE = re.compile(r'^\s+(?P<day>\d+)\s+(?P<max>\d+)\s+(?P<min>\d+).*')
+
 class Matcher(object):
+
     def __init__(self, regexp, fname):
-        self.regexp = re.compile(regexp)
+        self.regexp = regexp
         self.fname = fname
 
     def compute(self, func):
+        """Compute takes in input the function to apply to the result, as a callable
+        """
         res = {}
         for line in open(self.fname):
             m = self.regexp.match(line)
@@ -19,14 +25,14 @@ class Matcher(object):
 
 
 def footbal():
-    line_re = re.compile(r'\s+\d+\. (?P<team>\w+).*?(?P<for>\d+)\s+-\s+(?P<ag>\d+).*')
     res = {}
     for line in open('football.dat'):
-        m = line_re.match(line)
+        m = FOOTBAL_RE.match(line)
         res[m.group('team')] = int(m.group('for')) - int(m.group('ag'))
 
     return min(res, key=lambda x: res[x])
 
-print Matcher('^\s+(?P<day>\d+)\s+(?P<max>\d+)\s+(?P<min>\d+).*', 'weather.dat').compute(max)
-print Matcher('\s+\d+\. (?P<team>\w+).*?(?P<for>\d+)\s+-\s+(?P<ag>\d+).*', 'football.dat').compute(min)
+
+assert Matcher(WEATHER_RE, 'weather.dat').compute(max) == '9'
+assert Matcher(FOOTBAL_RE, 'football.dat').compute(min) == 'Leicester'
               
