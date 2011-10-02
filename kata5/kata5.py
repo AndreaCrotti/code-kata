d8a224370530ddbe18130abfadebf63541899b1d
Andrea Crotti <andrea.crotti.0@gmail.com>
2011-10-02 21:37:40 +0100

    start to solve the bloom filter problem

---
+++ kata5/kata5.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python2
+
+"""Implementation a spell checker using a bloom filter
+
+Bloom filters are very simple. Take a big array of bits, initially all
+zero. Then take the things you want to look up (in our case we’ll use
+a dictionary of words). Produce ‘n’ independent hash values for each
+word. Each hash is a number which is used to set the corresponding bit
+in the array of bits. Sometimes there’ll be clashes, where the bit
+will already be set from some other word. This doesn’t matter.
+
+"""
+
+DICT = "words"
+
+from array import array
+
+# create an hash for each word in the dictionary
+
+class NaiveChecker(object):
+
+    def __init__(self):
+        self.words = None
+
+    def populate(self):
+        # one liner to read everything
+        self.words = set(x.strip() for x in open(DICT).readlines())
+
+    def check(self, word):
+        return word in self.words
+
+
+class BloomChecker(object):
+    hash_dim = 32
+
+    def __init__(self):
+        self.hash = (2 ** self.hash_dim) - 1
+
+    def populate(self):
+        pass
+
+
+# add some ways to compute the speed of the different approaches
